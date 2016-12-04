82a028d061cace2774242d092780d466ee60be59
miladas01 <miladasghari2106@gmail.com>
2016-12-04 20:10:42 +0330

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component,NgZone } from '@angular/core';
+import {InfoProfile} from './info-profile';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent   {
+  infoProfile:InfoProfile = {name:'',email:''};
+  fShowInfo = false;
+  constructor(private zone: NgZone){}
+  ngAfterViewInit() {
+    gapi.signin2.render('my-signin2', {    
+        'onsuccess': param => this.onSignIn(param),
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'light'
+        
+    });
+  }
+  onSignIn(googleUser) {
+    this.fShowInfo = true;
+    var profile = googleUser.getBasicProfile();
+       // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+       console.log('Full Name: ' + profile.getName());
+       console.log('Given Name: ' + profile.getGivenName());
+       console.log('Family Name: ' + profile.getFamilyName());
+       console.log("Image URL: " + profile.getImageUrl());
+       console.log("Email: " + profile.getEmail());
+       this.zone.run(() => { this.infoProfile.name = profile.getName(),
+                             this.infoProfile.email = profile.getEmail(),
+                             console.log(profile)
+                             
+                           })   
+  };
+  signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut();
+    this.fShowInfo = false;
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {Component, Directive, Input, ViewChild,NgZone} from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
